6a990132124e22623791b1e4ea79077637fd5f1d
SHAHID <shahidhannure.sh@gmail.com>
2023-10-16 17:20:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NewsList from './NewsList';
+import NewsDetail from './NewsDetail';
 
 function App() {
   return (
-    <div className="App">
-      <h1>News Summarizer</h1>
-      <NewsList />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>News Summarizer</h1>
+        <Switch>
+          <Route path="/" exact component={NewsList} />
+          <Route path="/article/:id" component={NewsDetail} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
