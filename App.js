25d270a523e8f398a16cd12d7eb441419ea6d55c
SHAHID <shahidhannure.sh@gmail.com>
2023-10-16 11:06:59 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewsList from './NewsList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>News Summarizer</h1>
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
